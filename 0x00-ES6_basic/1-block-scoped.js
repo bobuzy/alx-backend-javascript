e7c0cd1ad9bc8c7def84708d7a26abe14454c03e
bobuzy <bobuzytech@gmail.com>
2024-10-23 14:10:50 +0000

    Add comments

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-	const task = false;
-	const task2 = true;
-	
-	return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  /* Create local variables */
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+  /* Return the initial task and task2 */
+
+  return [task, task2];
 }
