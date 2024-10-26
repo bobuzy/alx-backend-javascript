00b639dc1deb6c5120d4308eb3bcba16cd98311c
Benedict Abudu <115162603+bobuzy@users.noreply.github.com>
2024-10-26 14:03:44 +0100

    Update 1-block-scoped.js
    Remove if statement

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,5 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  /* Create local variables */
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-  }
-  /* Return the initial task and task2 */
-
   return [task, task2];
 }
